44c5a91329c6c9fd36e2bef50243df4084f18205
rk-10 <khanejarohan07@gmail.com>
2018-06-12 18:22:33 +0530

    listen and serve

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
+package main
 
+import (
+	"net/http"
+	"log"
+	"fmt"
+
+	"github.com/gorilla/mux"
+)
+
+func Login(w http.ResponseWriter, r *http.Request)  {
+	fmt.Println("Not implemented yet")
+}
+
+func main()  {
+	r := mux.NewRouter()
+	r.HandleFunc("/login", Login).Methods("GET")
+	http.ListenAndServe(":3000", r)
+	//http.Handle("/", r)
+	log.Fatal("Error")
+}
