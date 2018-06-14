f45555e33874b9c7caf6ca62c2ec3ef648053979
rk-10 <khanejarohan07@gmail.com>
2018-06-14 18:35:39 +0530

    created endpoints

--- main.go
+++ main.go
@@ -6,15 +6,44 @@ import (
 	"fmt"
 
 	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2/bson"
 )
 
-func Login(w http.ResponseWriter, r *http.Request)  {
-	fmt.Println("Not implemented yet")
+type Movie struct {
+	ID bson.ObjectId	`bson:"id" json:"id"`
+	Name string			`bson:"name" json:"name"`
+	CoverImage string	`bson:"cover_image" json:"cover_image"`
+	Description string	`bson:"description" json:"description"`
 }
+
+func AllMoviesEndPoint(w http.ResponseWriter, r *http.Request)  {
+	fmt.Println(w, "Not implemented yet")
+}
+
+func FindMovieEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "not implemented yet !")
+}
+
+func CreateMovieEndPoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "not implemented yet !")
+}
+
+func UpdateMovieEndPoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "not implemented yet !")
+}
+
+func DeleteMovieEndPoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "not implemented yet !")
+}
+
 
 func main()  {
 	r := mux.NewRouter()
-	r.HandleFunc("/login", Login).Methods("GET")
+	r.HandleFunc("/movies", AllMoviesEndPoint).Methods("GET")
+	r.HandleFunc("/movies", CreateMovieEndPoint).Methods("POST")
+	r.HandleFunc("/movies", UpdateMovieEndPoint).Methods("PUT")
+	r.HandleFunc("/movies", DeleteMovieEndPoint).Methods("DELETE")
+	r.HandleFunc("/movies/{id}", FindMovieEndpoint).Methods("GET")
 	http.ListenAndServe(":3000", r)
 	//http.Handle("/", r)
 	log.Fatal("Error")
