e19733df42b7759506e582f9be156e86d82e44ae
rk-10 <khanejarohan07@gmail.com>
2018-09-03 10:36:13 +0530

    updates test

--- tests/api_test.go
+++ tests/api_test.go
@@ -3,12 +3,15 @@ package tests
 import (
 	"testing"
 	"net/http"
+	"bytes"
 )
 
+const baseUrl  = "http://localhost:3000/"
 
-func TestGetAllMovies(t *testing.T)  {
-	req, _ := http.NewRequest("GET", "http://localhost:3000/movies", nil)
+func TestFindAllMovies(t *testing.T)  {
+	req, _ := http.NewRequest("GET", baseUrl + "movies", nil)
 	res, err := http.DefaultClient.Do(req)
+	t.Log(res)
 
 	if err != nil {
 		t.Error(err)
@@ -19,3 +22,25 @@ func TestGetAllMovies(t *testing.T)  {
 	}
 }
 
+func TestCreateMovie(t *testing.T)  {
+	var jsonStr = []byte(`
+{
+	"name": "Inception",
+	"description": "Enigma"
+}`)
+
+	req, _ := http.NewRequest("POST", baseUrl + "movies", bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res.StatusCode != 200 {
+		t.Error(`Wrong status code`)
+	}
+	}
