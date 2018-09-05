a333125e6fdc8d4a6d94cd7984dbc309545ce5c9
rk-10 <khanejarohan07@gmail.com>
2018-09-05 21:07:55 +0530

    updates test

--- tests/api_test.go
+++ tests/api_test.go
@@ -3,26 +3,15 @@ package tests
 import (
 	"testing"
 	"net/http"
+	"encoding/json"
+	"github.com/rk-10/REST-API/models"
 	"bytes"
 )
 
 const baseUrl  = "http://localhost:3000/"
+var movieId string
 
-func TestFindAllMovies(t *testing.T)  {
-	req, _ := http.NewRequest("GET", baseUrl + "movies", nil)
-	res, err := http.DefaultClient.Do(req)
-	t.Log(res)
-
-	if err != nil {
-		t.Error(err)
-	}
-
-	if res.StatusCode != 200 {
-		t.Error(`Wrong status code`)
-	}
-}
-
-func TestCreateMovie(t *testing.T)  {
+func TestCreateMovie(t *testing.T) {
 	var jsonStr = []byte(`
 {
 	"name": "Inception",
@@ -43,4 +32,31 @@ func TestCreateMovie(t *testing.T)  {
 	if res.StatusCode != 200 {
 		t.Error(`Wrong status code`)
 	}
+}
+
+
+func TestFindAllMovies(t *testing.T)  {
+	req, _ := http.NewRequest("GET", baseUrl + "movies", nil)
+	res, err := http.DefaultClient.Do(req)
+
+	var movie []models.Movie;
+	json.NewDecoder(res.Body).Decode(&movie)
+
+	movieId = movie[0].ID.Hex()
+	t.Log(movieId)
+
+	if err != nil {
+		t.Error(err)
 	}
+
+	if res.StatusCode != 200 {
+		t.Error(`Wrong status code`)
+	}
+
+
+}
+
+//
+//func TestUpdateMovie(t *testing.T)  {
+//
+//}
