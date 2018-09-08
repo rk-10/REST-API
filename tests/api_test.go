0475282e802a3ff006a9f4e0fef30126104136f2
rk-10 <khanejarohan07@gmail.com>
2018-09-08 23:47:39 +0530

    updates tests

--- tests/api_test.go
+++ tests/api_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 const baseUrl  = "http://localhost:3000/"
-var movieId string
+var Movie models.Movie
 
 func TestCreateMovie(t *testing.T) {
 	var jsonStr = []byte(`
@@ -23,8 +23,6 @@ func TestCreateMovie(t *testing.T) {
 
 	res, err := http.DefaultClient.Do(req)
 
-	defer res.Body.Close()
-
 	if err != nil {
 		t.Error(err)
 	}
@@ -35,6 +33,7 @@ func TestCreateMovie(t *testing.T) {
 }
 
 
+//This test fails if the above test fails.
 func TestFindAllMovies(t *testing.T)  {
 	req, _ := http.NewRequest("GET", baseUrl + "movies", nil)
 	res, err := http.DefaultClient.Do(req)
@@ -42,8 +41,12 @@ func TestFindAllMovies(t *testing.T)  {
 	var movie []models.Movie;
 	json.NewDecoder(res.Body).Decode(&movie)
 
-	movieId = movie[0].ID.Hex()
-	t.Log(movieId)
+	if len(movie) == 0 {
+		t.Error(`No movie found in DB`)
+	}
+
+	Movie = movie[0]
+	t.Log(movie)
 
 	if err != nil {
 		t.Error(err)
@@ -52,11 +55,50 @@ func TestFindAllMovies(t *testing.T)  {
 	if res.StatusCode != 200 {
 		t.Error(`Wrong status code`)
 	}
-
-
 }
 
-//
-//func TestUpdateMovie(t *testing.T)  {
+func TestFindMovieById(t *testing.T)  {
+
+	t.Log(baseUrl + "movies/" + Movie.ID.Hex())
+
+	req, _ := http.NewRequest("GET", baseUrl + "movies/" + Movie.ID.Hex() , nil)
+	res, err := http.DefaultClient.Do(req)
+
+	var movie models.Movie;
+	json.NewDecoder(res.Body).Decode(&movie)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res.StatusCode != 200 {
+		t.Error(`Wrong status code`)
+	}
+}
+
+
+func TestUpdateMovie(t *testing.T)  {
+	var jsonStr = []byte(`
+{	
+	"id": "` + Movie.ID.Hex() + `", 
+	"name": "Lala Land",
+	"description": "Oscar winning movie!"
+}`)
+
+	req, _ := http.NewRequest("PUT", baseUrl + "movies", bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res.StatusCode != 200 {
+		t.Error(`Wrong status code`)
+	}
+}
+
+//func TestDeleteMovie(t *testing.T)  {
 //
 //}
